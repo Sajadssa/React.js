7dd804eeb4a8bab356703fbd0381b2cb2a063e31
Sajadssa <sajadsaeediazad0007@gmail.com>
2022-12-10 16:58:09 +0330

    new changeshas update

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -4,8 +4,14 @@
 import { Fragment } from 'react';
 import styles from './product.module.css';
 import ProductList from '../ProductList/ProductList';
+import { useState } from 'react';
 //in here styles is an object so we can access that properties
 const Product = (props) => {
+  // const [userName, setuserName] = useState("");
+  // const changeHandler = (e) => {
+  //   setuserName(e.target.value);
+  // }
+
   // console.log(props);
   //note: Product is an object as same props is an object ,props is attributes component  that is parameter component  that for use dynamic component
   //develop Product component  passed argument or make dynamic component with props
@@ -14,22 +20,27 @@ const Product = (props) => {
   return (
     //when use from css module we can use access that properties
     <div className={styles.product} onClick={props.click}>
-  {/* how to dynamic in react */}
-  {/* <Fragment>
+      {/* how to dynamic in react */}
+      {/* <Fragment>
         <p >product name : {props.name}  </p>
         <p>price : {props.price}  </p>
   </Fragment> */}
-    <p >product name : {props.product.name}  </p>
-    <p>price : {props.product.price}  </p>
-    <span className={styles.value}>{props.product.quantity}</span>
-      <button className={`${styles.button} ${styles.inc}`} onClick={props.OnIncrement} >Increment</button>
-      <button className={`${styles.button} ${styles.dec}`} onClick={props.OnDecrement} >Decrement</button>
-    {/* for access nested data we can use children attribute */}
-   {props.children} 
-    {/* <p>Product price:{price}</p> */}
-    <button className={styles.button} onClick={props.onDelete}>Delete</button>
-  
-  </div>
+ 
+      <p >product name : {props.product.title}  </p>
+      <p>price : {props.product.price}  </p>
+      <span className={styles.value}>{props.product.quantity}</span>
+      <button className={`${styles.button} ${styles.inc}`} onClick={props.OnIncrement} >+</button>
+      {/* <input className={styles.input} onChange={props.onChange} type="text" value={props.product.title} /> */}
+    
+      <button className={`${styles.button} ${styles.dec}`} onClick={props.OnDecrement} >-</button>
+      <input className={styles.input} onChange={props.onChange} type="text" value={props.product.title} />
+
+      {/* for access nested data we can use children attribute */}
+      {props.children}
+      {/* <p>Product price:{price}</p> */}
+      <button className={styles.button} onClick={props.onDelete}>Delete</button>
+
+    </div>
   );
 };
 
--- src/component/ProductList/ProductList.js
+++ src/component/ProductList/ProductList.js
@@ -1,71 +1,71 @@
-import React, { useState,Component } from 'react';
-import Product from '../Product/Product';
+import React, { useState, Component } from "react";
+import Product from "../Product/Product";
 // import Product from './../Product/Product';
 class ProductList extends Component {
-    state = {
-        products: [
-            { title: "React.js", price: "90$", id: 1,quantity:1 },
-            { title: "javaScript", price: "80$", id: 2 ,quantity:2},
-            { title: "node.js", price: "95$", id: 3 ,quantity:3},
-        ] } ;
+  state = {
+    products: [
+      { title: "React.js", price: "90$", id: 1, quantity: 1 },
+      { title: "javaScript", price: "80$", id: 2, quantity: 2 },
+      { title: "node.js", price: "95$", id: 3, quantity: 3 },
+    ],
+  };
 
-        
-        //every state changed=>handler
-        removeHandler=(id)=>{
-            // console.log('clicked',id);
-            const filterProducts=this.state.products.filter((p)=>p.id!==id);
-            this.setState({products:filterProducts});
-        };
-    incrementHandler = (id) => {
-        console.log('increment',id);
-        const products=[...this.state.products];
-         const selectedItem=products.find((p)=>p.id===id);
-       selectedItem.quantity++;//mutate state
-       console.log(products);
-       this.setState({products:products});
-
-
-         
-           
-        }
-    decrementHandler = (id) => {
-        console.log('decrement',id);
-        const products=[...this.state.products];
-         const selectedItem=products.find((p)=>p.id===id);
-       selectedItem.quantity--;//mutate state
-       console.log(products);
-       this.setState({products:products});
-
-
-         
-           
-        }
-        
-    render() {
-
-        return (
-            <div>
-                {this.state.products.map((product, index) => {
-                    return (
-                        <Product
-                            // name={product.title}
-                            // price={product.price}
-                            // key={index}
-                            product={product}
-                            onDelete={() => this.removeHandler(product.id)}
-                            OnIncrement={() => this.incrementHandler(product.id)}
-                            OnDecrement={() => this.decrementHandler(product.id)}
-
-                        />
-                    );
-                })}
-
-            </div>
-
-        );
+  //every state changed=>handler
+  removeHandler = (id) => {
+    // console.log('clicked',id);
+    const filterProducts = this.state.products.filter((p) => p.id !== id);
+    this.setState({ products: filterProducts });
+  };
+  incrementHandler = (id) => {
+    // console.log('increment',id);
+    const products = [...this.state.products];
+    const selectedItem = products.find((p) => p.id === id);
+    selectedItem.quantity++; //mutate state
+    //    console.log(products);
+    this.setState({ products: products });
+  };
+  decrementHandler = (id) => {
+    // console.log('decrement',id);
+    const products = [...this.state.products];
+    const selectedItem = products.find((p) => p.id === id);
+    selectedItem.quantity--; //mutate state
+      this.setState({ products: products });
+    if (selectedItem.quantity < 1) {
+      const filterProducts = products.filter((p) => p.id !== id);
+      this.setState({ products: filterProducts });
     }
+    //    console.log(products);
+    //    this.setState({products:products});
+  };
+
+  changeHandler = (event, id) => {
+    console.log(event.target.value, id);
+    const products = [...this.state.products];
+    const selectedItem = products.find((p) => p.id === id);
+    selectedItem.title = event.target.value;
+    this.setState({ products: products });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.products.map((product, index) => {
+          return (
+            <Product
+              // name={product.title}
+              // price={product.price}
+              // key={index}
+              product={product}
+              onDelete={() => this.removeHandler(product.id)}
+              OnIncrement={() => this.incrementHandler(product.id)}
+              OnDecrement={() => this.decrementHandler(product.id)}
+              onChange={(e) => this.changeHandler(e, product.id)}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
-    
-    
- 
+
 export default ProductList;
